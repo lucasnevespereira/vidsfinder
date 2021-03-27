65d196fff27beed29255f1077a58fef742a5c296
lucas.pereiraneves <lucas.pereiraneves@grtgaz.com>
2021-03-27 19:04:06 +0100

    assigned response data to videos data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
   <div>
    Hello from vidsfinder
-   <SearchBar v-on:termChange=onTermChange /> 
-  </div>  
+   <SearchBar @termChange="onTermChange" /> 
+   <VideoList />
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
-const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+import VideoList from "./components/VideoList"
+const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList,
+  },
+  data() {
+    return {
+      videos: [],
+    };
   },
   methods: {
-    onTermChange: (searchTerm) => {
-      axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          type: 'video',
-          part: 'snippet',
-          q: searchTerm
-        }
-      }).then(response => console.log(response))
-    }
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then((response) => {
+          this.videos = response.data.items;
+        });
+    },
   },
 };
 </script>
