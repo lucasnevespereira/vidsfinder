faba5440a9dd4947fb8fcab49e22f035e7c99301
lucas.pereiraneves <lucas.pereiraneves@grtgaz.com>
2021-03-28 21:36:48 +0200

    handle video selection

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     </div>
     <div class="container">
     <SearchBar @termChange="onTermChange" /> 
-    <VideoList v-bind:videosArray="videos" />
+    <VideoDetail v-bind:video="selectedVideo" />
+    <VideoList @videoSelected="onVideoSelect" v-bind:videosArray="videos" />
     </div>
   </div>
 </template>
@@ -13,20 +14,26 @@
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
-import VideoList from "./components/VideoList"
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
 export default {
   name: 'App',
   components: {
     SearchBar,
     VideoList,
+    VideoDetail,
   },
   data() {
     return {
       videos: [],
+      selectedVideo: null,
     };
   },
   methods: {
+    onVideoSelect(video){
+      this.selectedVideo = video;
+    },
     onTermChange(searchTerm) {
       axios
         .get('https://www.googleapis.com/youtube/v3/search', {
---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="video">
+    Video Detail
+    {{video.snippet.title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoDetail",
+  props: ["video"]
+  
+}
+</script>
