983f1c9b590308020dd5e200a54791e42d8cbf2b
lucas.pereiraneves <lucas.pereiraneves@grtgaz.com>
2021-03-28 15:30:56 +0200

    looping trough received videos

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,18 @@
 <template>
   <ul>
-    VideoList
-    {{ videosArray.length }}
+    <VideoListItem v-for="v in videosArray" v-bind:video="v" :key="v.etag">
+      
+    </VideoListItem>
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem"
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem
+  },
   props: ['videosArray']
 }
 </script>
